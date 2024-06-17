ba39340adadd4e4a235f2a9076cde369488d44ae
Stephen Karukas <stephen.karukas@gmail.com>
2024-06-16 18:37:17 -0700

    Revamp code:
    - Refactor to object-oriented FunctionDescriptor
    - Capture ...spread syntax
    - Make this work for function subclasses
    - Add tests with assertions (even if only in the console).
    - Fix lint issues
    - Allow zero-argument functions
    - Fix some bugs with parsing comments.

--- main.js
+++ main.js
@@ -7,95 +7,131 @@
  * @property {regex} array - Finds parameters that are decomposed arrays
  * @type {Object}
  */
-const regex = {
-	arrow: /^\(?([\w\s,]+)\)?\s*=>/,
-	func: /(?:function|static|async)?\s*\w+\s?\(([\w\s=>"',:{}[\]\\]*)\)/,
+const _REGEX = {
+	arrow: /^\(?([^(^)]*)\)?\s*=>/,
+	func: /(?:function|static|async)?\s*\w+\s?\(([^(^)]*)\)/,
 	comments: /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg,
 	object: /^{([\w\s=>"',:{}[\]\\]*)*}$/,
 	array: /^\[[\w\s=>"',:{}[\]\\]*]$/,
 	json: /\{.*:\{.*:.*\}\}/g
 };
 
-class getParamNames {
+class ParamDescriptor {
+	constructor(name, hasDefault, rawName, destructureType=null) {
+		this.name = name;
+		this.rawName = rawName;
+		this.destructureType = destructureType;
+		this.hasDefault = hasDefault;
+	}
+	static fromString(paramString) {
+		let name, destructureType = null, hasDefault = false;
+		if (paramString.indexOf('=') !== -1) {
+			paramString = FunctionDescriptor.splitEquals(paramString)
+				.slice(0, -1)
+				.join('=')
+				.trim();
+			hasDefault = true;
+		}
+		const rawName = FunctionDescriptor.decompose(paramString);
+		if (typeof rawName == 'string') {
+			if (rawName.startsWith('...')) {
+				name = rawName.replace('...', '');
+				destructureType = 'spread';
+			} else {
+				name = rawName;
+			}
+		} else if (rawName instanceof Array) {
+			destructureType = 'array';
+		} else if (rawName instanceof Object) {
+			destructureType = 'object';
+		}
+		return new ParamDescriptor(name, hasDefault, rawName, destructureType);
+	}
+}
 
+// https://stackoverflow.com/questions/46118496/
+//  asyncfunction-is-not-defined-yet-mdn-documents-its-usage
+const AsyncFunction = Object.getPrototypeOf(async function () { }).constructor;
+
+class FunctionDescriptor {
 	/**
-	 * @constructor
-	 * @param {Function} f - The function for which parameter names should be returned
-	 */
+   * @constructor
+   * @param {Function} f - The function for which parameter names should be returned
+   */
 	constructor(f) {
 		this.f = f;
+		this.name = this.f.name;
+		this.isAsync = (this.f instanceof AsyncFunction);
+		this.parameters = FunctionDescriptor.parseArguments(this.f);
+		this.minArgs = this.f.length;
+		this.maxArgs = FunctionDescriptor._getMaxArgs(this.parameters);
+		this.functionString = FunctionDescriptor.stringify(this.f);
+		this.isArrowFunction = _REGEX.arrow.test(this.functionString);
+	}
+
+	static _getMaxArgs(parameters) {
+		const hasSpreadParam = parameters.some(p => p.destructureType == 'spread');
+		return hasSpreadParam ? Infinity : parameters.length;
 	}
 
 	/**
-	 * Little function wrapper to call the output result
-	 * @returns {*[]}
-	 * @public
-	 */
-	run () {
-		return this.params;
+   * The string version of the function passed to the constructor
+   * @name getParamNames#str
+   * @type {string}
+   * @private
+   */
+	static stringify(f) {
+		// Accounts for Function subclasses with overridden toString().
+		const boundToString = Function.prototype.toString.bind(f);
+		return boundToString().trim();
 	}
 
 	/**
-	 * The string version of the function passed to the constructor
-	 * @name getParamNames#str
-	 * @type {string}
-	 * @private
-	 */
-	get str () {
-		if (this._str) return this._str;
-		return this._str = this.f.toString().trim();
+   * @name getParamNames#arg
+   * @type {string}
+   * @private
+   */
+	static getArgsSection(functionString) {
+		if (functionString.replace(/\s/g, '').includes('{[nativecode]}')) {
+			throw new Error(
+				'FunctionDescriptors cannot be created for native functions. '
+       + 'Given: ' + functionString);
+		}
+		const match = functionString.match(_REGEX.arrow)
+      || functionString.match(_REGEX.func);
+		if (match && match.length) {
+			// match[0] will contain the full function definition, which we don't 
+			// want.
+			return match[1];
+		} else {
+			throw new Error(`Function parsing failed: ${functionString}.
+      Expected to match ${_REGEX.arrow} or ${_REGEX.func}`);
+		}
 	}
 
 	/**
-	 * @name getParamNames#arg
-	 * @type {string}
-	 * @private
-	 */
-	get arg () {
-		if (this._arg) return this._arg;
-		if (regex.arrow.test(this.str)) return this._arg = this.str.match(regex.arrow)[1];
-		else if (regex.func.test(this.str)) return this._arg = this.str.match(regex.func)[1];
-		throw new Error('Function parsing failed, review:\n' + this.str);
+   * Returns each parameter formatted perfectly - decompositions are in their object format
+   * Arrays - listed as an array of strings where each string is the decomposed name
+   * Objects - listed as an object with key: decomposed name, and value: true|default value
+   * @name getParaNames#params
+   * @type {*[]}
+   * @private
+   */
+	static parseArguments(f) {
+		const funcString = FunctionDescriptor.stringify(f);
+		const rawArgString = FunctionDescriptor.getArgsSection(funcString);
+		const cleanedArgString = rawArgString.replace(_REGEX.comments, '');
+		const splitArgStrings = FunctionDescriptor.splitArgsByCommas(cleanedArgString);
+		return splitArgStrings.map(ParamDescriptor.fromString);
 	}
 
 	/**
-	 * Returns the legitimately comma-separated argument, accounting for deconstructors
-	 * @name getParamNames#commaSeparated
-	 * @type {string[]}
-	 * @private
-	 */
-	get sep () {
-		if (this._sep) return this._sep;
-		return this._sep = getParamNames.splitCommas(this.arg.replace(regex.comments, ''));
-	}
-
-	/**
-	 * Returns each parameter formatted perfectly - decompositions are in their object format
-	 * Arrays - listed as an array of strings where each string is the decomposed name
-	 * Objects - listed as an object with key: decomposed name, and value: true|default value
-	 * @name getParaNames#params
-	 * @type {*[]}
-	 * @private
-	 */
-	get params () {
-		if (this._params) return this._params;
-		return this._params = this.sep
-			.map(i => {
-				if (i.indexOf('=') !== -1) i = getParamNames.splitEquals(i)
-					.slice(0, -1)
-					.join('=')
-					.trim();
-				return getParamNames.decompose(i);
-			});
-	}
-	
-	/**
-	 * Splits a single at any '=' sign, critically ignoring any '=>' signs
-	 * @param {string} str
-	 * @returns {string[]}
-	 * @private
-	 */
-	static splitEquals (str) {
+   * Splits a single at any '=' sign, critically ignoring any '=>' signs
+   * @param {string} str
+   * @returns {string[]}
+   * @private
+   */
+	static splitEquals(str) {
 		return str
 			.trim()
 			.split('=')
@@ -107,62 +143,69 @@ class getParamNames {
 	}
 
 	/**
-	 * Splits a string at any ',' sign, keeping objects and arrays intact
-	 * @param {string} str
-	 * @returns {string[]}
-	 * @private
-	 */
-	static splitCommas (str) {
+   * Splits a string at any ',' sign, keeping objects and arrays intact
+   * @param {string} str
+   * @returns {string[]}
+   * @private
+   */
+	static splitArgsByCommas(str) {
+		if (str.trim() == '') {
+			return [];
+		}
 		let obj = null;
 		let arr = str.split(',').map(i => i.trim());
 		return arr.reduce((acc, curr) => {
 			if (obj) acc[acc.length - 1] += ', ' + curr.trim();
 			else acc.push(curr.trim());
 			if (curr.includes('[')) obj = ']';
-			else if (curr.includes('{')) obj ='}';
+			else if (curr.includes('{')) obj = '}';
 			if (curr.endsWith(obj)) obj = null;
 			return acc;
 		}, []);
 	}
 
 	/**
-	 * Takes a string input to a function and decomposes it, like a compiler, to get variable names
-	 * Is recursive since decomposing Arrays and Objects might have further decomposition properties
-	 * @param {string} str
-	 * @returns {*[]}
-	 * @private
-	 */
-	static decompose (str) {
-		if (regex.object.test(str)) {
+   * Takes a string input to a function and decomposes it, like a compiler, to get variable names
+   * Is recursive since decomposing Arrays and Objects might have further decomposition properties
+   * @param {string} str
+   * @returns {*[]}
+   * @private
+   */
+	static decompose(str) {
+		if (_REGEX.object.test(str)) {
 			if (str === '{}') return {};
 			let obj = {};
 			const val = str.slice(1, -1).trim();
-			const entries = getParamNames.splitCommas(val);
+			const entries = FunctionDescriptor.splitArgsByCommas(val);
 			for (let entry of entries) {
-				let [key, value] = getParamNames.splitEquals(entry);
-				if (typeof value === 'undefined') [key, value] = key.split(':');
-				if (typeof value !== 'undefined') value = getParamNames.decompose(value.trim());
+				let [key, value] = FunctionDescriptor.splitEquals(entry);
+				if (typeof value === 'undefined') {
+					[key, value] = key.split(':');
+				}
+				if (typeof value !== 'undefined') {
+					value = FunctionDescriptor.decompose(value.trim());
+				}
 				obj[key.trim()] = value;
 			}
 			return obj;
-		} else
-		if (regex.array.test(str)) {
+		} else if (_REGEX.array.test(str)) {
 			if (str === '[]') return [];
-			let arr = getParamNames.splitCommas(str.slice(1, -1).trim());
+			let arr = FunctionDescriptor.splitArgsByCommas(str.slice(1, -1).trim());
 			for (let i = 0; i < arr.length; i++) {
-				arr[i] = getParamNames.decompose(arr[i]);
+				arr[i] = FunctionDescriptor.decompose(arr[i]);
 			}
 			return arr;
-		} else
-		if (str.trim() === 'true') return true;
-		else if (str.trim() === 'false') return false;
+		} else if (str.trim() === 'true') {
+			return true;
+		} else if (str.trim() === 'false') {
+			return false;
+		}
 		str = str.replace(/'/g, '');
 		return str;
 	}
 
 }
 
-module.exports = (f) => {
-	const Instance = new getParamNames(f);
-	return Instance.run();
+module.exports = function describeFunction(f) {
+	return new FunctionDescriptor(f);
 };
--- test.js
+++ test.js
@@ -1,30 +1,79 @@
 /*eslint no-console: "off"*/
 /*eslint-disable max-len */
 
-const getParamNames = require('./main.js');
+const describeFunction = require('./main.js');
 
-
-
+function assertHasSubset(obj, subset) {
+	let violations = [];
+	function _isSubset(superObj, subObj, path = '') {
+		return Object.keys(subObj).map(ele => {
+			const subpath = path + '.' + ele;
+			let matches;
+			if (subObj[ele] instanceof Object) {
+				matches = _isSubset(superObj[ele], subObj[ele], subpath);
+			} else {
+				const val = (superObj === undefined) ? NaN : superObj[ele];
+				matches = subObj[ele] === val;
+				if (!matches) {
+					violations.push(`${subpath}: ${val} != ${subObj[ele]}`);
+				}
+			}
+			return matches;
+		}).every(b => b);
+	}
+	console.assert(_isSubset(obj, subset), violations);
+}
 
 function f0(self, meme, init) {
 	return [self, meme, init];
 }
-console.log(getParamNames(f0)); // ["self", "meme", "init"]
-
-
-
+assertHasSubset(
+	describeFunction(f0),
+	{
+		minArgs: 3,
+		maxArgs: 3,
+		name: 'f0',
+		isArrowFunction: false,
+		isAsync: false,
+		parameters: [
+			{ name: 'self', destructureType: null },
+			{ name: 'meme', destructureType: null },
+			{ name: 'init', destructureType: null }
+		]
+	}
+);
 
 function f1(str = 'hello', arr = ['what\'s up']) {
 	return [str, arr];
 }
-console.log(getParamNames(f1)); // ["str", "arr"]
-
-
+assertHasSubset(
+	describeFunction(f1),
+	{
+		minArgs: 0,
+		name: 'f1',
+		isArrowFunction: false,
+		isAsync: false,
+		parameters: [
+			{ name: 'str', hasDefault: true, destructureType: null },
+			{ name: 'arr', hasDefault: true, destructureType: null },
+		]
+	}
+);
 
 let f2 = (meme, param) => [meme, param];
-console.log(getParamNames(f2)); // ["meme", "param"]
-
-
+assertHasSubset(
+	describeFunction(f2),
+	{
+		minArgs: 2,
+		name: 'f2',
+		isArrowFunction: true,
+		isAsync: false,
+		parameters: [
+			{ name: 'meme', hasDefault: false, destructureType: null },
+			{ name: 'param', hasDefault: false, destructureType: null },
+		]
+	}
+);
 
 async function f3(ids, {
 	fetch = false,
@@ -33,9 +82,21 @@ async function f3(ids, {
 } = {}) {
 	return [ids, fetch, filter.toString(), arr];
 }
-console.log(getParamNames(f3)); // [ 'ids', { fetch: false, filter: 'user => user', arr: [] } ]
 
-
+assertHasSubset(
+	describeFunction(f3),
+	{
+		minArgs: 1,
+		maxArgs: 2,
+		name: 'f3',
+		isArrowFunction: false,
+		isAsync: true,
+		parameters: [
+			{ name: 'ids', hasDefault: false, destructureType: null },
+			{ hasDefault: true, destructureType: 'object' },
+		]
+	}
+);
 
 async function f4({
 	item1,
@@ -47,4 +108,113 @@ async function f4({
 } = {}, [item3, item4, [item5, item6]]) {
 	return [item1, item2, item3, item4, item5, item6];
 }
-console.log(getParamNames(f4)); //[{"item2":{"prop1":true,"prop2":false,"prop3":["array","of","strings"]}},["item3","item4",["item5","item6"]]]
+
+assertHasSubset(
+	describeFunction(f4), 
+	{
+		minArgs: 0,
+		maxArgs: 2,
+		name: 'f4',
+		isArrowFunction: false,
+		isAsync: true,
+		parameters: [
+			{ hasDefault: true, destructureType: 'object' },
+			{ hasDefault: false, destructureType: 'array' },
+		]
+	}
+);
+
+// Works on classes too.
+class f5 {
+	constructor({
+		item1,
+		item2 = {
+			prop1: true,
+			prop2: false,
+			prop3: ['array', 'of', 'strings']
+		}
+	} = {}, [item3, item4, [item5, item6]]) {
+		return [item1, item2, item3, item4, item5, item6];
+	}
+}
+assertHasSubset(
+	describeFunction(f5),
+	{
+		minArgs: 0,
+		maxArgs: 2,
+		name: 'f5',
+		isArrowFunction: false,
+		isAsync: false,
+		parameters: [
+			{ hasDefault: true, destructureType: 'object' },
+			{ hasDefault: false, destructureType: 'array' },
+		]
+	}
+);
+
+// Works on Function subclasses with overridden toString.
+class FunctionTestClass extends Function {
+	toString() {
+		return '';
+	}
+}
+// Same definition as f4
+const f6 = new FunctionTestClass(
+	`{
+		item1,
+		item2 = { /* Get a load of this complicated [...arg] */
+			prop1: true,
+			prop2: false,
+			prop3: ['array', 'of', 'strings']
+		}
+	} = {}`,
+	'[item3, item4, [item5, item6]]', 
+	'return [item1, item2, item3, item4, item5, item6];');
+assertHasSubset(
+	describeFunction(f6),
+	{
+		minArgs: 0,
+		maxArgs: 2,
+		isArrowFunction: false,
+		isAsync: false,
+		parameters: [
+			{ hasDefault: true, destructureType: 'object' },
+			{ hasDefault: false, destructureType: 'array' },
+		]
+	}
+);
+
+// Recognizes spread syntax.
+function f7(
+	name,  // Comment. ,name, other {...args}, maybe
+	...otherArgs /* Block comment!!!!. {}[][args][] */
+) {
+	return [name, ...otherArgs];
+}
+assertHasSubset(
+	describeFunction(f7),
+	{
+		minArgs: 1,
+		maxArgs: Infinity,
+		name: 'f7',
+		isArrowFunction: false,
+		isAsync: false,
+		parameters: [
+			{ name: 'name', hasDefault: false, destructureType: null },
+			{ name: 'otherArgs', hasDefault: false, destructureType: 'spread' },
+		]
+	}
+);
+
+const f8 = (/* Nothing! */) => 'nothing';
+assertHasSubset(
+	describeFunction(f8),
+	{
+		minArgs: 0,
+		maxArgs: 0,
+		name: 'f8',
+		isArrowFunction: true,
+		isAsync: false,
+		parameters: []
+	}
+);
